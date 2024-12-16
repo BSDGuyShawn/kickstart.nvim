4d55970813e1be5fc62fa3989911daef33a6735f
Shawn Workman <shawn@bsdguy.com>
2024-12-16 11:40:47 -0800

    added odgmod plugin and odin stuff

--- init.lua
+++ init.lua
@@ -646,6 +646,7 @@ require('lazy').setup({
             },
           },
         },
+        ols = {},
       }
 
       -- Ensure the servers and tools above are installed
@@ -902,7 +903,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'odin' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
